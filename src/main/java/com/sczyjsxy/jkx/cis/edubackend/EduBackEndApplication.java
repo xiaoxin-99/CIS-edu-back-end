8453fc380e5f57ea5fbdcf3891d9fd776de5de55
lx <1605670114@qq.com>
2020-11-14 17:53:50 +0800

    init commit

--- src/main/java/com/sczyjsxy/jkx/cis/edubackend/EduBackEndApplication.java
+++ src/main/java/com/sczyjsxy/jkx/cis/edubackend/EduBackEndApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * @author lx
  */
-//@SpringBootApplication
+@SpringBootApplication
 public class EduBackEndApplication {
 
     public static void main(String[] args) {
