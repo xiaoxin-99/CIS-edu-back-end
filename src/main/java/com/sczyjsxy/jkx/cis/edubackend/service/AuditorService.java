24b7b565f459027d6c1617063e83eef06ec2e5a8
lx <1605670114@qq.com>
2020-11-25 19:11:14 +0800

    审核者服务接口（未实现）

--- src/main/java/com/sczyjsxy/jkx/cis/edubackend/service/AuditorService.java
+++ src/main/java/com/sczyjsxy/jkx/cis/edubackend/service/AuditorService.java
@@ -1,5 +1,9 @@
 package com.sczyjsxy.jkx.cis.edubackend.service;
 
+import com.sczyjsxy.jkx.cis.edubackend.model.dao.common.Adjustment;
+
+import java.util.List;
+
 /**
  * 审核者服务
  * @author lx
@@ -8,8 +12,38 @@ public interface AuditorService {
 
     /**
      * 系部批准调课
+     * @param id 调课编号
      */
-    void departmentApproved();
+    void departmentApproved(String id);
 
+    /**
+     * 系部拒绝调课
+     * @param id 调课编号
+     */
+    void departmentReject(String id);
+
+    /**
+     * 院批准调课
+     * @param id 调课编号
+     */
+    void collegeApproved(String id);
+
+    /**
+     * 院拒绝调课
+     * @param id 调课编号
+     */
+    void collegeReject(String id);
+
+    /**
+     * 系部待审核列表
+     * @return 返回列表
+     */
+    List<Adjustment> departmentList();
+
+    /**
+     * 院待审核列表
+     * @return 返回列表
+     */
+    List<Adjustment> collegeList();
 
 }
---
+++ src/main/java/com/sczyjsxy/jkx/cis/edubackend/service/impl/AuditorServiceImpl.java
@@ -0,0 +1,50 @@
+package com.sczyjsxy.jkx.cis.edubackend.service.impl;
+
+import com.sczyjsxy.jkx.cis.edubackend.mapper.AdjustmentMapper;
+import com.sczyjsxy.jkx.cis.edubackend.model.dao.common.Adjustment;
+import com.sczyjsxy.jkx.cis.edubackend.service.AuditorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 审核服务实现
+ * @author lx
+ */
+@Service
+public class AuditorServiceImpl implements AuditorService {
+
+    @Autowired
+    private AdjustmentMapper adjustmentMapper;
+
+    @Override
+    public void departmentApproved(String id) {
+
+    }
+
+    @Override
+    public void departmentReject(String id) {
+
+    }
+
+    @Override
+    public void collegeApproved(String id) {
+
+    }
+
+    @Override
+    public void collegeReject(String id) {
+
+    }
+
+    @Override
+    public List<Adjustment> departmentList() {
+        return null;
+    }
+
+    @Override
+    public List<Adjustment> collegeList() {
+        return null;
+    }
+}
